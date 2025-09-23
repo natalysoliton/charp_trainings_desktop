a05667d8521c1730e40173df021b942d00da9c67
Ermakova Natalia <natalysolitonsf@yandex.com>
2025-09-23 19:07:24 +0300

    added and updated project for task 18

---
+++ addressbook_tests_autoit/addressbook_tests_autoit/appmanager/ApplicationManager.cs
@@ -0,0 +1,42 @@
+﻿using AutoItX3Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace addressbook_tests_autoit
+{
+    public class ApplicationManager
+    {
+        public static string WINTITLE = "Free Address Book";
+        private AutoItX3 aux;
+        private GroupHelper groupHelper;
+        public ApplicationManager()
+        {
+            aux = new AutoItX3();
+            aux.Run(@"c:\Tools\AppsForTesting\AddressbookNative4\AddressBook.exe", "", aux.SW_SHOW);
+            aux.WinWait(WINTITLE);
+            aux.WinActivate(WINTITLE);
+            aux.WinWaitActive(WINTITLE);
+
+            groupHelper = new GroupHelper(this);
+        }
+        public void Stop()
+        {
+            aux.ControlClick(WINTITLE, "", "WindowsForms10.BUTTON.app.0.62e44910");
+        }
+        public AutoItX3 Aux
+        {
+            get { return aux;  }
+        }
+        public GroupHelper Groups {
+            get {
+                return groupHelper;
+            }
+}
+    }
+}
---
+++ addressbook_tests_autoit/addressbook_tests_autoit/appmanager/GroupHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace addressbook_tests_autoit
+{
+    public class GroupHelper : HelperBase
+    {
+        public static string GROUPWINTITLE = "Group editor";
+        public GroupHelper(ApplicationManager manager) : base(manager) { }
+
+        public void Add(GroupData newGroup)
+        {
+
+           
+            aux.ControlClick(WINTITLE, "", "WindowsForms10.BUTTON.app.0.62e44912"); // рефактор OpenGroupsDialogue();
+            aux.WinWait(GROUPWINTITLE); // рефактор OpenGroupsDialogue();
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.62e4493");
+            aux.Send(newGroup.Name);
+            aux.Send("{ENTER}");
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.62e4494"); //рефактор CloseGroupsDialogue();
+        }
+
+        public List<GroupData> GetGroupList()
+        {
+            OpenGroupsDialogue();
+            string count = aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.62e4491", "GetItemCount", "#0", "");
+            for (int i = 0; i < int.Parse(count); i++)
+            {
+               string item = aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.62e4491", "GetText", "#0|#" +i, "");
+                List.Add(new GroupData()
+                {
+                    Name = item
+                } );
+            }
+
+            CloseGroupsDialogue(); 
+        }
+    }
+}
