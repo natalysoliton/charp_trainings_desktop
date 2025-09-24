ef89cc7e157f51b221ae4995220c342a83c4766e
Ermakova Natalia <natalysolitonsf@yandex.com>
2025-09-24 22:15:21 +0300

    updated and fixed test

--- addressbook_tests_autoit/addressbook_tests_autoit/appmanager/GroupHelper.cs
+++ addressbook_tests_autoit/addressbook_tests_autoit/appmanager/GroupHelper.cs
@@ -13,30 +13,46 @@ namespace addressbook_tests_autoit
 
         public void Add(GroupData newGroup)
         {
+            OpenGroupsDialogue();
 
-           
-            aux.ControlClick(WINTITLE, "", "WindowsForms10.BUTTON.app.0.62e44912"); // рефактор OpenGroupsDialogue();
-            aux.WinWait(GROUPWINTITLE); // рефактор OpenGroupsDialogue();
+            // aux.ControlClick(WINTITLE, "", "WindowsForms10.BUTTON.app.0.62e44912"); // рефактор OpenGroupsDialogue();
+            // aux.WinWait(GROUPWINTITLE); // рефактор OpenGroupsDialogue();
             aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.62e4493");
             aux.Send(newGroup.Name);
             aux.Send("{ENTER}");
-            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.62e4494"); //рефактор CloseGroupsDialogue();
+
+            CloseGroupsDialogue();
+            // aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.62e4494"); //рефактор CloseGroupsDialogue();
+        }
+
+        private void OpenGroupsDialogue()
+        {
+            aux.ControlClick(WINTITLE, "", "WindowsForms10.BUTTON.app.0.62e44912");
+            aux.WinWait(GROUPWINTITLE);
+        }
+
+        private void CloseGroupsDialogue()
+        {
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.62e4494");
         }
 
         public List<GroupData> GetGroupList()
         {
+            List<GroupData> list = new List<GroupData>(); // создаем список для результатов
+
             OpenGroupsDialogue();
             string count = aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.62e4491", "GetItemCount", "#0", "");
             for (int i = 0; i < int.Parse(count); i++)
             {
                string item = aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.62e4491", "GetText", "#0|#" +i, "");
-                List.Add(new GroupData()
+                list.Add(new GroupData()
                 {
                     Name = item
                 } );
             }
 
-            CloseGroupsDialogue(); 
+            CloseGroupsDialogue();
+            return list; // возвращаем заполненный список
         }
     }
 }
